c8a00a3e2f15a8d8eb37ded2dfceae55d9847604
Brett V. Forsgren <brett.forsgren@outlook.com>
2016-09-13 11:24:35 -0700

    don't create documentation for internal-only helper functions

--- src/helper_functions.rs
+++ src/helper_functions.rs
@@ -13,6 +13,7 @@ use ::Color;
 use ::enums::*;
 use ::tables::Layer;
 
+#[doc(hidden)]
 pub fn verify_code(expected: i32, actual: i32) -> io::Result<()> {
     if expected == actual {
         Ok(())
@@ -22,6 +23,7 @@ pub fn verify_code(expected: i32, actual: i32) -> io::Result<()> {
     }
 }
 
+#[doc(hidden)]
 pub fn as_bool(v: i16) -> bool {
     v == 1
 }
@@ -36,6 +38,7 @@ fn f64_to_adjusted_duration(f: f64) -> Duration {
         + Duration::seconds(1)
 }
 
+#[doc(hidden)]
 pub fn as_datetime_local(date: f64) -> DateTime<Local> {
     // dates are represented as the fractional number of days elapsed since December 31, 1899.
     if date == 0.0 {
@@ -46,6 +49,7 @@ pub fn as_datetime_local(date: f64) -> DateTime<Local> {
     }
 }
 
+#[doc(hidden)]
 pub fn as_datetime_utc(date: f64) -> DateTime<UTC> {
     // dates are represented as the fractional number of days elapsed since December 31, 1899.
     if date == 0.0 {
@@ -56,26 +60,31 @@ pub fn as_datetime_utc(date: f64) -> DateTime<UTC> {
     }
 }
 
+#[doc(hidden)]
 pub fn as_double_local(date: DateTime<Local>) -> f64 {
     let epoch = Local.ymd(1900, 1, 1).and_hms(0, 0, 0);
     let duration = date - epoch;
     (duration.num_seconds() as f64 / 24.0 / 60.0 / 60.0) + 2415021f64
 }
 
+#[doc(hidden)]
 pub fn as_double_utc(date: DateTime<UTC>) -> f64 {
     let epoch = UTC.ymd(1900, 1, 1).and_hms(0, 0, 0);
     let duration = date - epoch;
     (duration.num_seconds() as f64 / 24.0 / 60.0 / 60.0) + 2415021f64
 }
 
+#[doc(hidden)]
 pub fn duration_as_double(duration: Duration) -> f64 {
     duration.num_seconds() as f64
 }
 
+#[doc(hidden)]
 pub fn as_duration(d: f64) -> Duration {
     Duration::seconds(d as i64)
 }
 
+#[doc(hidden)]
 pub fn as_u32(s: String) -> io::Result<u32> {
     let mut result = 0;
     for c in s.chars() {
@@ -103,10 +112,12 @@ pub fn as_u32(s: String) -> io::Result<u32> {
     Ok(result)
 }
 
+#[doc(hidden)]
 pub fn as_handle(h: u32) -> String {
     format!("{:X}", h)
 }
 
+#[doc(hidden)]
 pub fn as_uuid(s: String) -> io::Result<Uuid> {
     match Uuid::parse_str(s.as_str()) {
         Ok(uuid) => Ok(uuid),
@@ -114,42 +125,52 @@ pub fn as_uuid(s: String) -> io::Result<Uuid> {
     }
 }
 
+#[doc(hidden)]
 pub fn as_short(b: bool) -> i16 {
     if b { 1 } else { 0 }
 }
 
+#[doc(hidden)]
 pub fn uuid_string(u: &Uuid) -> String {
     format!("{}", u)
 }
 
+#[doc(hidden)]
 pub fn default_if_empty(val: &String, default: &str) -> String {
     if val.is_empty() { String::from(default) } else { val.clone() }
 }
 
+#[doc(hidden)]
 pub fn ensure_positive_or_default(val: f64, default: f64) -> f64 {
     if val <= 0.0 { default } else { val }
 }
 
+#[doc(hidden)]
 pub fn ensure_positive_or_default_i32(val: i32, default: i32) -> i32 {
     if val <= 0 { default } else { val }
 }
 
+#[doc(hidden)]
 pub fn ensure_positive_or_default_i16(val: i16, default: i16) -> i16 {
     if val <= 0 { default } else { val }
 }
 
+#[doc(hidden)]
 pub fn get_writable_linetype_name<'a>(val: &'a str) -> &'a str {
     if val.is_empty() { "CONTINUOUS" } else { val }
 }
 
+#[doc(hidden)]
 pub fn clipping_from_bool(b: bool) -> Option<XrefClippingBoundaryVisibility> {
     XrefClippingBoundaryVisibility::from_i16(if b { 1 } else { 0 })
 }
 
+#[doc(hidden)]
 pub fn bool_from_clipping(c: XrefClippingBoundaryVisibility) -> bool {
     c != XrefClippingBoundaryVisibility::NotDisplayedNotPlotted
 }
 
+#[doc(hidden)]
 pub fn version_from_string(v: String) -> io::Result<AcadVersion> {
     match &*v {
         "MC0.0" => Ok(AcadVersion::Version_1_0),
@@ -195,6 +216,7 @@ pub fn version_from_string(v: String) -> io::Result<AcadVersion> {
     }
 }
 
+#[doc(hidden)]
 pub fn string_from_version(v: &AcadVersion) -> String {
     String::from(
         match v {
@@ -221,6 +243,7 @@ pub fn string_from_version(v: &AcadVersion) -> String {
     })
 }
 
+#[doc(hidden)]
 pub fn parse_bool(s: String) -> io::Result<bool> {
     match parse_short(s) {
         Ok(0) => Ok(false),
@@ -229,6 +252,7 @@ pub fn parse_bool(s: String) -> io::Result<bool> {
     }
 }
 
+#[doc(hidden)]
 pub fn parse_double(s: String) -> io::Result<f64> {
     match s.parse::<f64>() {
         Ok(d) => Ok(d),
@@ -236,6 +260,7 @@ pub fn parse_double(s: String) -> io::Result<f64> {
     }
 }
 
+#[doc(hidden)]
 pub fn parse_int(s: String) -> io::Result<i32> {
     match s.parse::<i32>() {
         Ok(i) => Ok(i),
@@ -243,6 +268,7 @@ pub fn parse_int(s: String) -> io::Result<i32> {
     }
 }
 
+#[doc(hidden)]
 pub fn parse_long(s: String) -> io::Result<i64> {
     match s.parse::<i64>() {
         Ok(l) => Ok(l),
@@ -250,6 +276,7 @@ pub fn parse_long(s: String) -> io::Result<i64> {
     }
 }
 
+#[doc(hidden)]
 pub fn parse_short(s: String) -> io::Result<i16> {
     match s.parse::<i16>() {
         Ok(s) => Ok(s),
@@ -257,6 +284,7 @@ pub fn parse_short(s: String) -> io::Result<i16> {
     }
 }
 
+#[doc(hidden)]
 pub fn trim_trailing_newline(s: &mut String) {
     if s.ends_with('\n') {
         s.pop();
@@ -266,6 +294,7 @@ pub fn trim_trailing_newline(s: &mut String) {
     }
 }
 
+#[doc(hidden)]
 pub fn read_color_value(layer: &mut Layer, color: i16) -> Color {
     layer.is_layer_on = color >= 0;
     Color::from_raw_value(color.abs())
