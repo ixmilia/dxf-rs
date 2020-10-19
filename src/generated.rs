b9103acdf5eab6ec45b41817bf281c8ae62b8df5
Brett V. Forsgren <brett.forsgren@outlook.com>
2020-10-19 13:48:41 -0700

    work around rust-analyzer bug

--- src/generated.rs
+++ src/generated.rs
@@ -1,1 +1,20 @@
-include!(concat!(env!("OUT_DIR"), "/generated/mod.rs"));
+// Due to https://github.com/rust-analyzer/rust-analyzer/issues/4075, rust-analyzer can't navigate to relative `mod`
+// statements.  To make working with rust-analyzer easier in this repo, we'll manually specify the modules.
+
+//include!(concat!(env!("OUT_DIR"), "/generated/mod.rs"));
+
+pub mod entities {
+    include!(concat!(env!("OUT_DIR"), "/generated/entities.rs"));
+}
+
+pub mod header {
+    include!(concat!(env!("OUT_DIR"), "/generated/header.rs"));
+}
+
+pub mod objects {
+    include!(concat!(env!("OUT_DIR"), "/generated/objects.rs"));
+}
+
+pub mod tables {
+    include!(concat!(env!("OUT_DIR"), "/generated/tables.rs"));
+}
