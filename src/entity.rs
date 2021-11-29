3f40cec67cea546edccd60ef9aa0f4da13d72a03
Brett V. Forsgren <brett.forsgren@outlook.com>
2021-11-29 14:41:14 -0700

    only write extrusion for `INSERT` on R13+

--- src/entity.rs
+++ src/entity.rs
@@ -2801,6 +2801,42 @@ mod tests {
     }
 
     #[test]
+    fn write_insert_no_extrusion_on_r12() {
+        let mut drawing = Drawing::new();
+        drawing.header.version = AcadVersion::R12;
+        let mut ins = Insert::default();
+        ins.extrusion_direction = Vector::new(1.0, 2.0, 3.0);
+        let ent = Entity::new(EntityType::Insert(ins));
+        drawing.add_entity(ent);
+        assert_not_contains_pairs(
+            &drawing,
+            vec![
+                CodePair::new_f64(210, 1.0),
+                CodePair::new_f64(220, 2.0),
+                CodePair::new_f64(230, 3.0),
+            ],
+        );
+    }
+
+    #[test]
+    fn write_insert_extrusion_on_r13() {
+        let mut drawing = Drawing::new();
+        drawing.header.version = AcadVersion::R13;
+        let mut ins = Insert::default();
+        ins.extrusion_direction = Vector::new(1.0, 2.0, 3.0);
+        let ent = Entity::new(EntityType::Insert(ins));
+        drawing.add_entity(ent);
+        assert_contains_pairs(
+            &drawing,
+            vec![
+                CodePair::new_f64(210, 1.0),
+                CodePair::new_f64(220, 2.0),
+                CodePair::new_f64(230, 3.0),
+            ],
+        );
+    }
+
+    #[test]
     fn round_trip_insert_with_attributes() {
         let mut drawing = Drawing::new();
         let mut ins = Insert::default();
