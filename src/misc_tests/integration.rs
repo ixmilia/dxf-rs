51083dceaf1a083bbb469ad918c0b534c24637a5
Brett V. Forsgren <brett.forsgren@outlook.com>
2021-12-01 10:45:07 -0700

    add autocad block roundtrip integration test

--- src/misc_tests/integration.rs
+++ src/misc_tests/integration.rs
@@ -2,8 +2,8 @@ use crate::entities::*;
 use crate::enums::*;
 use crate::*;
 
-use std::fs::{create_dir_all, read_to_string, remove_dir_all};
-use std::path::Path;
+use std::fs::{create_dir_all, read_to_string, remove_dir_all, write};
+use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::thread::panicking;
 use std::time::SystemTime;
@@ -29,7 +29,8 @@ impl Drop for Oda {
 }
 
 impl Oda {
-    pub fn convert_drawing(&self, drawing: &Drawing, version: AcadVersion) -> Drawing {
+    pub fn convert_drawing(&self, drawing: &mut Drawing, version: AcadVersion) -> Drawing {
+        drawing.header.version = version;
         drawing
             .save_file(&format!("{}/drawing.dxf", self.input_path))
             .unwrap();
@@ -131,6 +132,135 @@ macro_rules! require_oda {
     }};
 }
 
+struct AutoCAD {
+    temp_path: String,
+    acad_path: String,
+}
+
+impl Drop for AutoCAD {
+    fn drop(&mut self) {
+        if panicking() {
+            println!("Test drawings at '{}'", self.temp_path);
+        } else {
+            remove_dir_all(Path::new(&self.temp_path)).unwrap();
+        }
+    }
+}
+
+impl AutoCAD {
+    pub fn convert_drawing(&self, drawing: &mut Drawing, version: AcadVersion) -> Drawing {
+        drawing.header.version = version;
+        drawing
+            .save_file(&format!("{}/input.dxf", self.temp_path))
+            .unwrap();
+        // e.g.,
+        //   acad.exe /b script.scr
+        let mut input_file = PathBuf::new();
+        input_file.push(&self.temp_path);
+        input_file.push("input.dxf");
+        let input_file = input_file.to_str().unwrap();
+
+        let mut output_file = PathBuf::new();
+        output_file.push(&self.temp_path);
+        output_file.push("output.dxf");
+        let output_file = output_file.to_str().unwrap();
+
+        let mut script_contents = String::new();
+        script_contents.push_str("FILEDIA 0\n");
+        script_contents.push_str(&format!("DXFIN \"{}\"\n", input_file));
+        script_contents.push_str(&format!(
+            "DXFOUT \"{}\" V {} 16\n",
+            output_file,
+            &AutoCAD::version_string(version),
+        ));
+        script_contents.push_str("FILEDIA 1\n");
+        script_contents.push_str("QUIT Y\n");
+        let mut script_path = PathBuf::new();
+        script_path.push(&self.temp_path);
+        script_path.push("script.scr");
+        let script_path = script_path.to_str().unwrap();
+        write(&script_path, &script_contents).expect("failed to write script file");
+
+        let mut acad_convert = Command::new(&self.acad_path)
+            .arg("/b")
+            .arg(&script_path)
+            .spawn()
+            .expect("Failed to spawn acad");
+        let exit_code = acad_convert.wait().expect("Failed to wait for acad");
+        let mut error_messages = String::from("");
+        for entry in glob(format!("{}/*.err", &self.temp_path).as_str())
+            .expect("failed to glob for acad error logs")
+        {
+            match entry {
+                Ok(path) if path.is_file() => {
+                    error_messages.push_str(format!("{}:\n", path.to_str().unwrap()).as_str());
+                    let error_contents = read_to_string(path).unwrap();
+                    error_messages.push_str(error_contents.as_str());
+                    error_messages.push_str("\n\n");
+                }
+                Ok(_) => (),
+                Err(_) => (),
+            }
+        }
+        if !error_messages.is_empty() {
+            panic!("Error converting files:\n{}", error_messages);
+        }
+
+        assert!(exit_code.success());
+        Drawing::load_file(&format!("{}/output.dxf", self.temp_path)).unwrap()
+    }
+    fn version_string(version: AcadVersion) -> String {
+        let s = match version {
+            AcadVersion::R12 => "R12",
+            AcadVersion::R2000 => "2000",
+            AcadVersion::R2004 => "2004",
+            AcadVersion::R2007 => "2007",
+            AcadVersion::R2010 => "2010",
+            AcadVersion::R2013 => "2013",
+            AcadVersion::R2018 => "2018",
+            _ => panic!("Unsupported acad version {}", version),
+        };
+        String::from(s)
+    }
+}
+
+macro_rules! require_acad {
+    () => {{
+        // Find AutoCAD.  Final path looks something like:
+        //   C:\Program Files\Autodesk\AutoCAD 2016\acad.exe
+        let mut full_acad_path = None;
+        for entry in glob("C:/Program Files/Autodesk/AutoCAD */acad.exe")
+            .expect("failed to glob for acad.exe")
+        {
+            match entry {
+                Ok(path) if path.is_file() => full_acad_path = Some(path),
+                Ok(_) => (),
+                Err(_) => (),
+            }
+        }
+        if full_acad_path == None {
+            // didn't find AutoCAD directory
+            return;
+        }
+        let full_acad_path = full_acad_path.unwrap();
+
+        // make and report temporary directory
+        let nanos = format!(
+            "{}",
+            SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_nanos()
+        );
+        let temp_path = format!("{}/integration-tests/{}", env!("OUT_DIR"), nanos);
+        create_dir_all(Path::new(&temp_path)).unwrap();
+        AutoCAD {
+            temp_path,
+            acad_path: String::from(full_acad_path.to_str().unwrap()),
+        }
+    }};
+}
+
 #[test]
 fn simple_line_can_be_read_by_oda() {
     let oda = require_oda!();
@@ -144,13 +274,87 @@ fn simple_line_can_be_read_by_oda() {
         }),
     });
 
-    let round_tripped = oda.convert_drawing(&drawing, AcadVersion::R2000);
+    let round_tripped = oda.convert_drawing(&mut drawing, AcadVersion::R2000);
     let entities = round_tripped.entities().collect::<Vec<_>>();
     assert_eq!(1, entities.len());
     match entities[0].specific {
         EntityType::Line(ref line) => {
             assert_eq!(Point::new(1.0, 2.0, 3.0), line.p1);
             assert_eq!(Point::new(4.0, 5.0, 6.0), line.p2);
+        }
+        _ => panic!("expected a line"),
+    }
+}
+
+#[test]
+fn simple_line_can_be_read_by_acad_r12() {
+    let acad = require_acad!();
+    let mut drawing = Drawing::new();
+    drawing.add_entity(Entity {
+        common: Default::default(),
+        specific: EntityType::Line(Line {
+            p1: Point::new(1.0, 2.0, 3.0),
+            p2: Point::new(4.0, 5.0, 6.0),
+            ..Default::default()
+        }),
+    });
+
+    let round_tripped = acad.convert_drawing(&mut drawing, AcadVersion::R12);
+    let entities = round_tripped.entities().collect::<Vec<_>>();
+    assert_eq!(1, entities.len());
+    match entities[0].specific {
+        EntityType::Line(ref line) => {
+            assert_eq!(Point::new(1.0, 2.0, 3.0), line.p1);
+            assert_eq!(Point::new(4.0, 5.0, 6.0), line.p2);
+        }
+        _ => panic!("expected a line"),
+    }
+}
+
+#[test]
+fn block_insert_can_be_read_by_acad_r12() {
+    let acad = require_acad!();
+    let mut drawing = Drawing::new();
+    let mut block = Block::default();
+    block.name = "my-block-name".to_string();
+    block.entities.push(Entity {
+        common: Default::default(),
+        specific: EntityType::Line(Line::new(
+            Point::new(0.0, 0.0, 0.0),
+            Point::new(1.0, 1.0, 0.0),
+        )),
+    });
+    drawing.add_block(block);
+    let mut insert = Insert::default();
+    insert.name = "my-block-name".to_string();
+    insert.location = Point::new(3.0, 3.0, 0.0);
+    drawing.add_entity(Entity {
+        common: Default::default(),
+        specific: EntityType::Insert(insert),
+    });
+
+    let round_tripped = acad.convert_drawing(&mut drawing, AcadVersion::R12);
+    let entities = round_tripped.entities().collect::<Vec<_>>();
+
+    // verify insert
+    assert_eq!(1, entities.len());
+    match entities[0].specific {
+        EntityType::Insert(ref insert) => {
+            assert_eq!("my-block-name", insert.name.to_lowercase());
+        }
+        _ => panic!("expected an insert"),
+    }
+
+    // verify line
+    let block = round_tripped
+        .blocks()
+        .filter(|b| b.name.to_lowercase() == "my-block-name")
+        .collect::<Vec<_>>()[0];
+    assert_eq!(1, block.entities.len());
+    match block.entities[0].specific {
+        EntityType::Line(ref line) => {
+            assert_eq!(Point::new(0.0, 0.0, 0.0), line.p1);
+            assert_eq!(Point::new(1.0, 1.0, 0.0), line.p2);
         }
         _ => panic!("expected a line"),
     }
