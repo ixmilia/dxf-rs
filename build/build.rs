bb68381f3a45db7d4ca173b9d8bd2c3ec5550eed
Bruce Mitchener <bruce.mitchener@gmail.com>
2023-10-14 09:53:13 +0700

    clippy: Remove unneceesary `allow(clippy::all)` from generated code.
    The lints have all been addressed, so this can be removed now.

--- build/build.rs
+++ build/build.rs
@@ -37,7 +37,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut file = File::create(generated_dir.join("mod.rs")).ok().unwrap();
     file.write_all("// The contents of this file are automatically generated and should not be modified directly.  See the `build` directory.
 
-#[allow(clippy::all)]
 pub mod entities;
 pub mod header;
 pub mod objects;
